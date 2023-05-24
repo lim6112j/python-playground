7bbec31ad0dd0e0a3862f44319584b5586354c3e
ben lim <lim6112j@gmail.com>
2023-05-24 19:03:56 +0900

    position option

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ window = tk.Tk()
 window.title("demo")
 window.geometry('600x300')
 # title
-title_label = ttk.Label(master=window, text='hello', font='Calibri 24 bold')
+title_label = ttk.Label(
+    master=window, text='miles to kilometers', font='Calibri 24 bold')
 title_label.pack()
 # input_field
 input_frame = ttk.Frame(master=window)
 entry = ttk.Entry(master=input_frame)
 button = ttk.Button(master=input_frame, text='Convert')
-entry.pack()
-button.pack()
+entry.pack(side='left')
+button.pack(side='left')
 input_frame.pack()
 # run
 window.mainloop()
